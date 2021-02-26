9efe8dd7fc0aa36d6cb23d6454e89181ff3dba66
Diego Leonardo Duran Castaños <a2019119811@estudiantes.upsa.edu.bo>
2021-02-25 20:04:57 -0400

    Se creo el contralador 'Calculator', junto con las funciones: Add,Subtract,Multiply,Divison; en postman

---
+++ Sol.Calculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿namespace ApiCalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //Proceso en Postman:
+        //Se ejecuta el programa y se copia la url en postman
+        // Ejemplo https://localhost:44362/api/Calculator/Division
+        [HttpPost]
+        [Route("Add")]//Al tener varias funciones esta es una referencia para el Postman; para que sepa que es esta funcion en especifico
+        public int Add([FromHeader] int num_a, [FromHeader] int num_b) //Creamos una funcion; FromHeader = Para recibir desde el Header los valores
+        {
+            return num_a + num_b;
+        }
+
+        [HttpPost]
+        [Route("Subtract")]
+        public int Subtract([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a - num_b;
+        }
+
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a * num_b;
+        }
+
+        [HttpPost]
+        [Route("Division")]
+        public int Division([FromHeader] int num_a,[FromHeader]int num_b)
+        {
+            return num_a / num_b;
+        }
+    }
+}
