1a2e3908c828fc5695723a49dbbd92cd96b841e1
Diego Leonardo Duran Castaños <a2019119811@estudiantes.upsa.edu.bo>
2021-02-27 10:01:18 -0400

    Agregamos  HttpGet de las funciones de add,multiply,subtract,division

--- Sol.Calculadora/ApiCalculadora/Controllers/CalculatorController.cs
+++ Sol.Calculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -9,9 +9,17 @@
         //Proceso en Postman:
         //Se ejecuta el programa y se copia la url en postman
         // Ejemplo https://localhost:44362/api/Calculator/Division
+
         [HttpPost]
         [Route("Add")]//Al tener varias funciones esta es una referencia para el Postman; para que sepa que es esta funcion en especifico
-        public int Add([FromHeader] int num_a, [FromHeader] int num_b) //Creamos una funcion; FromHeader = Para recibir desde el Header los valores
+        public int Add([FromHeader] int num_a, [FromHeader] int num_b) //Creamos una funcion; FromHeader = Para recibir desde el Header los valores, o desde la GUI de postman
+        {
+            return num_a + num_b;
+        }
+
+        [HttpGet]
+        [Route("Add")]
+        public int Add_2( int num_a, int num_b) 
         {
             return num_a + num_b;
         }
@@ -23,18 +31,49 @@
             return num_a - num_b;
         }
 
-        [HttpPost]
+        [HttpGet]
+        [Route("Subtract")]
+        public int Subtract_2(int num_a, int num_b)
+        {
+            return num_a - num_b;
+        }
+
+        [HttpGet]
         [Route("Multiply")]
-        public int Multiply([FromHeader] int num_a, [FromHeader] int num_b)
+        public int Multiply_2(int num_a, int num_b)
         {
             return num_a * num_b;
+        }
+
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader]int num_a,[FromHeader] int num_b)
+        {
+            return num_a * num_b;
+        }
+
+        [HttpGet]
+        [Route("Division")]
+        public int Division_2(int num_a, int num_b)
+        {
+            if (num_b == 0)
+            {
+                return 0;
+            }
+            else
+                return num_a / num_b;
         }
 
         [HttpPost]
         [Route("Division")]
         public int Division([FromHeader] int num_a,[FromHeader]int num_b)
         {
-            return num_a / num_b;
-        }
+            if (num_b == 0)
+            {
+                return 0;
+            }
+            else
+                return num_a / num_b;
+        } 
     }
 }
